9ba6937afb0618e31f60dcdfd4010ff6ebcc6db8
Knut Liseter <knut.lisaeter@nersc.no>
2015-07-22 19:33:44 +0200

    Added archv reading

--- modeltools/hycom/io.py
+++ modeltools/hycom/io.py
@@ -3,6 +3,7 @@ import numpy
 import struct
 import sys
 import logging
+import re
 
 # Set up logger
 _loglevel=logging.DEBUG
@@ -16,11 +17,14 @@ logger.addHandler(ch)
 
 
 
-class ZAFileError(Exception) :
+class AFileError(Exception) :
    pass
 
-class ZAFile(object) :
-   def __init__(self,idm,jdm,filename,action,mask=False,real4=True,endian="native") :
+
+
+class AFile(object) :
+   _huge = 2.0**100
+   def __init__(self,idm,jdm,filename,action,mask=False,real4=True,endian="big") :
       self._idm = idm
       self._jdm = jdm 
       self._filename = filename 
@@ -29,18 +33,20 @@ class ZAFile(object) :
       self._real4= real4
       self._endian= endian
       if self._action.lower() not in ["r","w"]  :
-         raise ZAFileError("action argument must be either r(ead) or w(rite)")
+         raise AFileError("action argument must be either r(ead) or w(rite)")
       if self._endian.lower() not in ["little","big","native"]  :
-         raise ZAFileError("action argument must be either native, little ort big")
+         raise AFileError("action argument must be either native, little ort big")
 
       
       if self._endian.lower() == "native" :
-         self._swap_endian = False
+         self._endian=sys.byteorder
+
+      if self._endian.lower() == "big" :
+         self._endian_structfmt = ">"
       else :
-         self._swap_endian = str(self._endian) <> str(sys.byteorder)
+         self._endian_structfmt = "<"
 
       logging.debug("Endianness set to %s",self._endian)
-      logging.debug("Byteswapping is   %s"%str(self._swap_endian))
 
 
       self._zaiost()
@@ -60,13 +66,15 @@ class ZAFile(object) :
    def _zaiopf(self) :
       # Open .a and .b file
       self._filea = open(self._filename+".a",self._action+"b")
-      self._fileb = open(self._filename+".b",self._action)
 
 
 
-   def zaiowr_a(self,h,mask) :
+   def zaiowr_a(self,h,mask,record=None) :
       w=numpy.ones(self._n2drec)*self._spval
       w[0:self._idm*self._jdm] = h.flatten('F') # Fortran order
+
+      if record is not None :
+         raise AFileError("Seeked writes not yet supported")
 
       # Calc min and mask
       if self._mask :
@@ -76,66 +84,210 @@ class ZAFile(object) :
          hmin=h(I).min()
          J=numpy.where(~tmp)
          w[J] = self._spval
-
       else :
          hmax=h.max()
          hmin=h.min()
 
       if self._real4 :
-         w = w.astype(numpy.float32)
          struct_fmt="f"
       else :
-         w = w.astype(numpy.float64)
          struct_fmt="d"
-
-
-
-      # Write to file, handle endianness ?
-      if self._swap_endian :
-         w = w.byteswap()
-      #print w[0]
-
-      # This is okay for sequential access. For accessing specific records, use fseek
-      #self._filea.write(w)
-      #
-      #binpack=struct.pack("=%d%s"%(w.size,struct_fmt),*w[:])
-      #w.write(binpack)
-      #
-      w.tofile(self._filea)
-
+      binpack=struct.pack("%s%d%s"%(self._endian_structfmt,w.size,struct_fmt),*w[:])
+      self._filea.write(binpack)
       return hmin,hmax
+
+
+   def zaiord_a(self,mask,record) :
+
+      # Seek to correct record and read
+      self.zaioseek_a(record)
+      if self._real4 :
+         raw = self._filea.read(self.n2drec*4)
+         fmt =  "%s%df"%(self._endian_structfmt,self.n2drec)
+      else :
+         raw = self._filea.read(self.n2drec*8)
+         fmt =  "%s%dd"%(self._endian_structfmt,self.n2drec)
+      w =  numpy.array(struct.unpack(fmt,raw))
+      w=numpy.ma.masked_where(w>self._huge*.5,w)
+
+      w=w[0:self.idm*self.jdm]
+      w.shape=(self.jdm,self.idm)
+
+      return w
+
+
+
+   def zaioseek_a(self,record) :
+      # Seek to correct record and read
+      if self._real4 :
+         self._filea.seek(record*self.n2drec*4)
+      else :
+         self._filea.seek(record*self.n2drec*8)
+      return
 
 
    def close(self) :
       self._filea.close()
-      self._fileb.close()
+
 
    @property
    def n2drec(self):
       return self._n2drec
 
 
+   @property
+   def idm(self):
+      return self._idm
 
-class ZAFileRegionalGrid(ZAFile) :
+
+   @property
+   def jdm(self):
+      return self._jdm
+
+
+class BFile(object) :
+
+   def __init__(self,filename,action) :
+      self._filename=filename+".b"
+      self._action=action
+      self._fileb = open(self._filename,self._action)
+
+   def close(self) :
+      self._fileb.close()
+
+   def scanitem(self,item=None,conversion=None) :
+      line = self._fileb.readline().strip()
+      if item is not None :
+         print "^(.*)'(%-6s)'[ ]*="%item,line
+         m=re.match("^(.*)'(%-6s)'[ ]*="%item,line)
+      else :
+         m=re.match("^(.*)'(.*)'[ ]*=",line)
+      if m :
+         if conversion :
+            value = conversion(m.group(1))
+         return m.group(2),value
+      else :
+         return None,None
+   def readline(self) :
+      return self._fileb.readline()
+
+
+
+
+class ABFileRegionalGrid(BFile) :
    def __init__(self,idm,jdm,filename,action,mask=False,real4=True,endian="native",mapflg=-1) :
-      super(ZAFileRegionalGrid,self).__init__(idm,jdm,filename,action,mask=mask,real4=real4,endian=endian)
+      super(BFile,self).__init__(filename,action)
+      self._filea.__init__(idm,jdm,filename,action,mask=mask,real4=real4,endian=endian)
 
-      # Regional file .b header
-      self._fileb.write("%5d    %s\n"%(self._idm,"'idm   '"))
-      self._fileb.write("%5d    %s\n"%(self._idm,"'jdm   '"))
-      self._fileb.write("%5d    %s\n"%(mapflg,"'mapflg'"))
+      if action == "r" :
+         # Regional file .b header
+         self._fileb.write("%5d    %s\n"%(self._idm,"'idm   '"))
+         self._fileb.write("%5d    %s\n"%(self._idm,"'jdm   '"))
+         self._fileb.write("%5d    %s\n"%(mapflg,"'mapflg'"))
 
-
-
-   def zaiowr(self,field,mask,fieldname) :
+   def writefield(self,field,mask,fieldname) :
       hmin,hmax = self.zaiowr_a(field,mask)
       self.zaiowr_b(fieldname,hmin,hmax)
 
-
    def zaiowr_b(self,fieldname,hmin,hmax) :
-      self._fileb.write("%4s:  min,max =%16.5f%16.5f\n"%(fieldname,hmin,hmax))
+      self.write("%4s:  min,max =%16.5f%16.5f\n"%(fieldname,hmin,hmax))
+
+
+
+class ABFileArchv(BFile) :
+   fieldkeys=["field","step","day","k","dens","min","max"]
+   def __init__(self,filename,mask=False,real4=True,endian="big") :
+      super(ABFileArchv,self).__init__(filename,"r")
+      self.readheader() # Sets self._idm, self._jdm
+      self._filea = AFile(self._idm,self._jdm,filename,"r",mask=mask,real4=real4,endian=endian)
+
+   #def set_metadata(self,idm,jdm,
+
+
+
+   def readheader(self) :
+      self._header=[]
+      self._header.append(self.readline())
+      self._header.append(self.readline())
+      self._header.append(self.readline())
+      self._header.append(self.readline())
+
+      item,self._iversn = self.scanitem(item="iversn",conversion=int)
+      item,self._iexpt  = self.scanitem(item="iexpt",conversion=int)
+      item,self._yrflag = self.scanitem(item="yrflag",conversion=int)
+      item,self._idm    = self.scanitem(item="idm",conversion=int)
+      item,self._jdm    = self.scanitem(item="jdm",conversion=int)
+
+      # Get list of fields from .b file
+      #field       time step  model day  k  dens        min              max
+      #montg1   =      67392    351.000  1 25.000   0.0000000E+00   0.0000000E+00
+      #
+      self._fields={}
+      line=self.readline()
+      line=self.readline().strip()
+      i=0
+      while line :
+         elems = re.split("[ =]+",line)
+         self._fields[i] = dict(zip(self.fieldkeys,[el.strip() for el in elems]))
+         for k in self.fieldkeys :
+            if k in ["min","max","dens","day"] :
+               self._fields[i][k] = float(self._fields[i][k])
+            elif k in ["k","step"] :
+               self._fields[i][k] = int(self._fields[i][k])
+         i+=1
+         line=self.readline().strip()
+
+
+   def readfield(self,fieldname,level) :
+      """ Read field corresponding to fieldname and level from archive file"""
+      record = None
+      for i,d in self._fields.items() :
+         if d["field"] == fieldname and level == d["k"] :
+            print d["field"]
+            record=i
+      if record  is not None :
+         w = self.readrecord(record) 
+      else :
+         w = None
+      return w
+
+
+   def readrecord(self,record) :
+      """ Read single record from archive file"""
+      return self._filea.zaiord_a(1,record)
+
+
+   @property
+   def fieldnames(self) :
+      return set([elem["field"] for elem in self._fields.values()])
+
+
+   @property
+   def levels(self) :
+      return set([elem["k"] for elem in self._fields.values()])
+      
       
 
+
+
+         
+
+
+
+#      self._fileb.write("%5d    %s\n"%(self._idm,"'idm   '"))
+#      self._fileb.write("%5d    %s\n"%(self._idm,"'jdm   '"))
+#      self._fileb.write("%5d    %s\n"%(mapflg,"'mapflg'"))
+#   22    'iversn' = hycom version number x10
+#   10    'iexpt ' = experiment number x10
+#    1    'yrflag' = days in year flag
+#  800    'idm   ' = longitudinal array size
+#  880    'jdm   ' = latitudinal  array size
+#
+##   def zaiowr(self,field,mask,fieldname) :
+##      hmin,hmax = self.zaiowr_a(field,mask)
+##      self.zaiowr_b(fieldname,hmin,hmax)
+#      
+#   open
 
 
 def write_regional_grid(grid) :
@@ -145,7 +297,7 @@ def write_regional_grid(grid) :
    qlon,qlat=grid.qgrid()
 
 
-   regf = ZAFileRegionalGrid(grid.Nx,grid.Ny,"regional.grid","w",endian="native")
+   regf = AFile(grid.Nx,grid.Ny,"regional.grid","w",endian="native")
    regf.zaiowr(plon,plon,"plon")
    regf.zaiowr(plat,plat,"plat")
    regf.zaiowr(qlon,qlon,"qlon")
@@ -175,11 +327,7 @@ def write_regional_grid(grid) :
 
 
 
-def write_newpos(grid,endian="native") :
-   if endian.lower() == "native" :
-      swap_endian = False
-   else :
-      swap_endian = str(endian) <> str(sys.byteorder)
+def write_newpos(grid) :
    logging.debug("Endianness set to %s",endian)
    logging.debug("Byteswapping is   %s"%str(swap_endian))
 
@@ -191,25 +339,16 @@ def write_newpos(grid,endian="native") :
    # Write to file, handle endianness ?
    plon = plon.astype(numpy.float64)
    plat = plat.astype(numpy.float64)
-   if swap_endian :
-      plon = plon.byteswap()
-      plat = plat.byteswap()
-      nrec = nrec.byteswap()
+   plon_binpack=struct.pack("=%dd"%plon.size,*plon[:])
+   plat_binpack=struct.pack("=%dd"%plat.size,*plat[:])
+   nrec_binpack=struct.pack("=i",plon.size)
 
-   print nrec,nrec.byteswap()
-   print map(hex,nrec)
-   print map(hex,nrec.byteswap())
-   print map(hex,nrec.byteswap().byteswap())
-
-
+   # Fortran sequential unformatted access requires number of elements as first and last 4 bytes...
    fid=open("newpos.uf","wb")
-
-   # Fortran sequential unformatted access requires number of elements as first 
-   nrec.tofile(fid)
-   plat.tofile(fid)
-   plon.tofile(fid)
-   nrec.tofile(fid)
-   #fid.write(sdruct.pack("i4",nrec))
+   fid.write(nrec_binpack)
+   fid.write(plon_binpack)
+   fid.write(plat_binpack)
+   fid.write(nrec_binpack)
    fid.close()
 
 
