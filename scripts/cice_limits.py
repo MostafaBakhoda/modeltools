5a1e280d81a5ab651220e23052a3d0f51b1f2da7
Knut Liseter <knut.lisaeter@nersc.no>
2016-05-23 14:02:36 +0200

    fixes

--- scripts/cice_limits.py
+++ scripts/cice_limits.py
@@ -1,41 +1,65 @@
 #!/usr/bin/env python
 ##!/usr/bin/python -E
 import modeltools.hycom
-import namelist_python
+import f90nml
 import argparse
 import datetime
+import numpy
 import os
 
 
-def main(start_time,end_time,restart) :
-
-
+def main(start_time,end_time,infile,init) :
    fnml = "ice_in"
-   nml  = namelist_python.read_namelist_file(fnml)
-   dt   = nml.groups["setup_nml"]["dt"]
-   print dt,type(dt)
-   #nml.groups["setup_nml"]["year_init"] =start_time.year
-   #open("tst_in","w").write(nml.dump())
+   nml  = f90nml.read(fnml)
+   dt   = nml["setup_nml"]["dt"]
+
+   # Integration time, initial year
+   tint      = end_time - start_time
+   tint_secs = tint.days*86400. + tint.seconds
+   #year_init = start_time.year
+   year_init = 1958
+   npt=int(numpy.floor(tint_secs/dt))
+   
+   # Seconds lapsed into this year
+   sec0=start_time - datetime.datetime(year_init,1,1,0,0,0)
+   sec0=sec0.days*86400. + sec0.seconds
+   istep0=int(numpy.floor(sec0/dt))-1
+
+   if istep0 < 0 :
+      #year_init=year_init-1
+      #sec0=start_time - datetime.datetime(year_init,1,1,0,0,0)
+      #sec0=sec0.days*86400. + sec0.seconds
+      #istep0=int(numpy.floor(sec0/dt))-1
+      raise NameError,"negative istep0, adjust year_init"
 
 
-   init_year = start_time.year
-   deltat=start_time - datteim
+   nml["setup_nml"]["year_init"] = year_init
+   nml["setup_nml"]["istep0"] = istep0
+   nml["setup_nml"]["npt"]=npt
+   print "year_init = ",nml["setup_nml"]["year_init"]
+   print "istep0    = ",nml["setup_nml"]["istep0"]
+   print "npt       = ",nml["setup_nml"]["npt"]
 
+   if init :
+      nml["setup_nml"]["runtype"]="initial"
+      nml["setup_nml"]["restart"]=False
+      nml["setup_nml"]["use_restart_time"]=False
+   else :
+      nml["setup_nml"]["runtype"]="continue"
+      nml["setup_nml"]["restart"]=True
+      nml["setup_nml"]["use_restart_time"]=True
 
+   nml.write("ice_in",force=True)
 
-#   fday,fhour,fsecond = modeltools.hycom.datetime_to_ordinal(start_time,bp["yrflag"])
-#   lday,lhour,lsecond = modeltools.hycom.datetime_to_ordinal(end_time,  bp["yrflag"])
-#   #print start_time,fday,fhour,fsecond,start_time.year
-#
-#   # NB Add 1 to fday, ldat - they start from 0 but hycom
-#   # starts from 1
-#   fdtime=modeltools.hycom.dayfor(start_time.year,fday,fhour,yrflag)+fsecond/86400.
-#   ldtime=modeltools.hycom.dayfor(end_time.year  ,lday,lhour,yrflag)+lsecond/86400.
-#   #print fdtime, start_time,fday,fhour,fsecond,start_time.year
-#
-#   f = open("limits", "w")
-#   f.write( "%14.5f %14.5f" % ( (rfac*fdtime), ldtime ))
-#   f.close
+   path_restart= os.path.join("./", nml["setup_nml"]["restart_dir"])
+   if not os.path.isdir(path_restart) :
+      os.mkdir(path_restart)
+
+   path_pointer= os.path.join("./", os.path.dirname(nml["setup_nml"]["pointer_file"]))
+   if not os.path.isdir(path_pointer) :
+      os.mkdir(path_pointer)
+
+   print "test"
 
 if __name__ == "__main__" :
    class DateTimeParseAction(argparse.Action) :
@@ -46,9 +70,9 @@ if __name__ == "__main__" :
    parser.add_argument('--init',       action="store_true", default=False)
    parser.add_argument('start_time', action=DateTimeParseAction, help='Start time in UTC zone. Format = YYYY-mm-ddTHH:MM:SS')
    parser.add_argument('end_time',   action=DateTimeParseAction, help='Stop  time in UTC zone. Format = YYYY-mm-ddTHH:MM:SS')
+   parser.add_argument('infile',     help='CICE namelist')
    args = parser.parse_args()
-
-   main(args.start_time,args.end_time,args.init)
+   main(args.start_time,args.end_time,args.infile,args.init)
 
 
 
